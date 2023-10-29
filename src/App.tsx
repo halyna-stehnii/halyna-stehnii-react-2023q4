e23a2a0f17c198298fa972b8f3f4d7697b6c0000
Halyna Stehnii <galyna.stegniy@gmail.com>
2023-10-29 20:53:34 +0100

    feat: implement SearcResults component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,71 @@
-import { useState } from 'react';
+import { Component } from 'react';
+import SearchResults from './components/SearchResults';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+export type SearchResult = {
+  count: number;
+  next: string;
+  previous: string;
+  results: any[];
+};
 
-  return (
-    <>
-      <div>Hello world</div>
-    </>
-  );
+export type Person = {
+  birth_year: string;
+  created: string;
+  edited: string;
+  eye_color: string;
+  films: string[];
+  gender: string;
+  hair_color: string;
+  height: string;
+  homeworld: string;
+  mass: string;
+  name: string;
+  skin_color: string;
+  species: string[];
+  starships: string[];
+  vehicles: string[];
+};
+
+interface State {
+  searchTerm: string;
+  searchResults: SearchResult;
+}
+
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      searchResults: { count: 0, next: '', previous: '', results: [] },
+    };
+  }
+
+  componentDidMount() {
+    this.fetchSearchResults();
+  }
+
+  fetchSearchResults() {
+    const searchUrl = 'https://swapi.dev/api/people/';
+
+    fetch(searchUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ searchResults: data });
+      })
+      .catch((error) => {
+        console.error('Error fetching search results', error);
+      });
+  }
+
+  render() {
+    console.log(this.state.searchResults.results);
+    return (
+      <div className="App">
+        <SearchResults searchResults={this.state.searchResults} />
+      </div>
+    );
+  }
 }
 
 export default App;
